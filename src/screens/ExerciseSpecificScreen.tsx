8808a5556666e8ddf32f4898c8add2f2a0e41d7e
Naresh Batta <battanaresh456@gmail.com>
2026-02-02 23:43:10 +0530

    fix: prettier issue fixes

--- src/screens/ExerciseSpecificScreen.tsx
+++ src/screens/ExerciseSpecificScreen.tsx
@@ -1,41 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Header from '../components/common/Header'
-import { useRoute } from '@react-navigation/native'
-interface WorkoutProps{
-  item:{
-    id?:string,
-    exerciseName?:string,
-    thumbNail?:string,
-    videoUrl?:string,
-  }
-}
+import {Text, View} from 'react-native';
+import React from 'react';
+import Header from '../components/common/Header';
+import {useRoute} from '@react-navigation/native';
+// interface WorkoutProps{
+//   item:{
+//     id?:string,
+//     exerciseName?:string,
+//     thumbNail?:string,
+//     videoUrl?:string,
+//   }
+// }
 const ExerciseSpecificScreen = () => {
-    const route = useRoute()
-    console.log(route?.params)
-    // const workOutId=workOutsData?.filter((list)=>list?.id===route?.params?.id)
-    const {item}:any=route?.params
+  const route = useRoute();
+  console.log(route?.params);
+  // const workOutId=workOutsData?.filter((list)=>list?.id===route?.params?.id)
+  const {item}: any = route?.params;
   return (
     <View>
-        <Header title={item?.exerciseName}/>
+      <Header title={item?.exerciseName} />
       <Text>ExerciseSpecificScreen</Text>
-              {/* //   <Video 
+      {/* //   <Video
               //   repeat
               //   resizeMode='cover'
               //   // Can be a URL or a local file.
               //   source={{uri:item?.thumbNail}}
-              //   // Store reference  
+              //   // Store reference
               //   ref={videoRef}
-              //   // Callback when remote video is buffering                                      
+              //   // Callback when remote video is buffering
               //   onBuffer={()=>console.log("bufffer")}
-              //   // Callback when video cannot be loaded              
-              //   onError={()=>console.log("error")}               
+              //   // Callback when video cannot be loaded
+              //   onError={()=>console.log("error")}
               //   style={{width:"100%",height:200}}
               //  /> */}
     </View>
-  )
-}
+  );
+};
 
-export default ExerciseSpecificScreen
+export default ExerciseSpecificScreen;
 
-const styles = StyleSheet.create({})
+// const styles = StyleSheet.create({})
--- src/utils/server.js
+++ src/utils/server.js
@@ -5,12 +5,11 @@
 // const isEmulator=DeviceInfo.isEmulator()
 // export const socket = io.connect(isEmulator?'http://10.0.2.2:3000/':'http:localhost:3000')
 
+import {Platform} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+import {io} from 'socket.io-client';
 
-import { Platform } from "react-native";
-import DeviceInfo from "react-native-device-info";
-import { io } from "socket.io-client";
-
-let baseURL = "";
+let baseURL = '';
 
 const setupSocket = async () => {
   const isEmulator = await DeviceInfo.isEmulator();
@@ -19,18 +18,18 @@ const setupSocket = async () => {
   const localNetworkIP = 'http://192.168.0.13:3000';
 
   baseURL =
-    Platform.OS === "android"
+    Platform.OS === 'android'
       ? isEmulator
-        ? "http://10.0.2.2:3000"
+        ? 'http://10.0.2.2:3000'
         : localNetworkIP
       : isEmulator
-      ? "http://localhost:3000"
+      ? 'http://localhost:3000'
       : localNetworkIP;
 
   // Export socket after URL is resolved
   return io(baseURL);
 };
-
+console.log('typeof', typeof setupSocket);
 export const BaseUrl = baseURL;
-export const socket = io.connect('http://192.168.0.13:3000')
+export const socket = io.connect('http://192.168.0.13:3000');
 // export const socketPromise = setupSocket(); // use await in your component to get socket
